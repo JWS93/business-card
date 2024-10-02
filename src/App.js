506a1c0267652eba4849224aef8abaa62f44bde4
JUSTIN SCOTT <JUSTINSCOTT820@GMAIL.COM>
2024-10-01 20:15:52 -0700

    troubleshooting font awesome issues

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/about.js'
 import Interests from './components/interests.js'
 
 
+
 function App() {
   return (
     <div className="App container">
--- src/components/personal-info.js
+++ src/components/personal-info.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import profile from '../images/profile.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 function PersonalInfo () {
@@ -11,7 +12,7 @@ function PersonalInfo () {
       <h4>Full-Stack Developer</h4>
       <a href = "https://github.com/JWS93/" className = "website">Github</a>
       <div className = "buttons justify-content-center row mx-3 my-2">
-        <button className = "btn btn-light rounded email-btn col-10 mx-2 ">Email</button>
+        <button className = "btn btn-light rounded email-btn col-10 mx-2 "><FontAwesomeIcon icon="fa-solid fa-envelope" />Email</button>
       </div>
     </div>
     
